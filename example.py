dc35b60ac38a618010b77f3810e46182f3275dbb
kesha1225 <samedov03@mail.ru>
2020-01-26 16:24:04 +0300

    changed example

--- example.py
+++ example.py
@@ -11,13 +11,20 @@ app.app_name = "SUPER_APP_3000_MEGA_COOL"
 
 @app.index()
 async def index(request: Request):
-    return HTMLResponse("hello")
+    return HTMLResponse("<h1>hello</h1>")
 
 
 @app.get("/<param>")
-async def index(request: Request):
-    print(request.path_dict)
-    return TextResponse("hi")
+async def some_param_route(request: Request):
+    param = request.path_dict["param"]
+    return f"your path now - /{param}", 200
+
+
+@app.get("/<name>/<age>")
+async def some_json_route(request: Request):
+    name = request.path_dict["name"]
+    age = request.path_dict["age"]
+    return {"user": {"name": name, "age": age}}
 
 
 app.run()
